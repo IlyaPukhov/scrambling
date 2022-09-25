8248f4686d705c303b1b4548748235ef23c8d7da
Ilya Pukhov <dinamond2003@gmail.com>
2022-09-26 02:22:43 +0300

    Added GUI

--- src/com/ilyap/Scrambler/ScramblerRunner.java
+++ src/com/ilyap/Scrambler/ScramblerRunner.java
@@ -1,20 +1,28 @@
 package com.ilyap.Scrambler;
 
-public class ScramblerRunner {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ScramblerRunner extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/scramble.fxml")));
+        primaryStage.getIcons().add(
+                new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/icon.png"))));
+        primaryStage.setTitle("Скремблирование");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        String sourceCode = "111111000000000000000000000000000000000000000011";
-        System.out.println("Исходный код:                             " + sourceCode);
-
-        String scrambled = DefaultScrambler.scramble(sourceCode);
-        System.out.println("\nКод, полученный стандартным скремблером:  " + scrambled);
-
-        String descrambled = DefaultScrambler.descramble(scrambled);
-        System.out.println("Дескремблированный код:                   " + descrambled);
-
-        String newScrambled = EnhancedScrambler.scramble(sourceCode);
-        System.out.println("\nКод, полученный уникальным скремблером:   " + newScrambled);
-
-        String newDescrambled = EnhancedScrambler.descramble(newScrambled);
-        System.out.println("Дескремблированный код:                   " + newDescrambled);
+        launch(args);
     }
 }
---
+++ src/com/ilyap/Scrambler/ScramblingController.java
@@ -0,0 +1,44 @@
+package com.ilyap.Scrambler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+
+public class ScramblingController {
+
+    @FXML
+    private TextField defaultDescramblerField;
+
+    @FXML
+    private TextField defaultScramblerField;
+
+    @FXML
+    private TextField enhancedDescramblerField;
+
+    @FXML
+    private TextField enhancedScramblerField;
+
+    @FXML
+    private Button scramblingButton;
+
+    @FXML
+    private TextField sourceField;
+
+    @FXML
+    void initialize() {
+        scramblingButton.setOnAction(actionEvent -> setResults(sourceField.getText()));
+        sourceField.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                setResults(sourceField.getText());
+            }
+        });
+    }
+
+    public void setResults(String sourceCode) {
+        defaultScramblerField.setText(DefaultScrambler.scramble(sourceCode));
+        defaultDescramblerField.setText(DefaultScrambler.descramble(defaultScramblerField.getText()));
+        enhancedScramblerField.setText(EnhancedScrambler.scramble(sourceCode));
+        enhancedDescramblerField.setText(EnhancedScrambler.descramble(enhancedScramblerField.getText()));
+    }
+}
