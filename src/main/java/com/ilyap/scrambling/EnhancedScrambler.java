18d51036738918507b8ef806d4bb86ca2c3182e6
IlyaPukhov <dinamond2003@gmail.com>
2022-11-18 21:46:46 +0300

    Replace Enhanced scramble algorithm

--- src/main/java/com/ilyap/scrambling/EnhancedScrambler.java
+++ src/main/java/com/ilyap/scrambling/EnhancedScrambler.java
@@ -12,9 +12,7 @@ public final class EnhancedScrambler {
         int[] result = new int[sourceArr.length];
 
         for (int i = 0; i < source.length(); i++) {
-            if (i >= 48) {
-                result[i] = sourceArr[i] ^ result[i - 4] ^ result[i - 44] ^ result[i - 48];
-            } else if (i >= 44) {
+            if (i >= 44) {
                 result[i] = sourceArr[i] ^ result[i - 4] ^ result[i - 44];
             } else if (i >= 4) {
                 result[i] = sourceArr[i] ^ result[i - 4];
@@ -29,9 +27,7 @@ public final class EnhancedScrambler {
         int[] scrambledArr = getArr(scrambled);
         int[] result = new int[scrambledArr.length];
         for (int i = 0; i < scrambled.length(); i++) {
-            if (i >= 48) {
-                result[i] = scrambledArr[i] ^ scrambledArr[i - 4] ^ scrambledArr[i - 44] ^ scrambledArr[i - 48];
-            } else if (i >= 44) {
+            if (i >= 44) {
                 result[i] = scrambledArr[i] ^ scrambledArr[i - 4] ^ scrambledArr[i - 44];
             } else if (i >= 4) {
                 result[i] = scrambledArr[i] ^ scrambledArr[i - 4];
